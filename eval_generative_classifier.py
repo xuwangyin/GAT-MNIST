5621bee8b0fcff2e5229f6c6a0231694e8cfffbc
Xuwang Yin <xuwangyin@gmail.com>
2019-10-07 20:11:56 -0400

    add attack with xent loss

--- eval_generative_classifier.py
+++ eval_generative_classifier.py
@@ -1,13 +1,14 @@
 import argparse
 import sys
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.ERROR)
 from models import MadryClassifier, BayesClassifier, PGDAttackClassifier
 from eval_utils import BaseDetectorFactory, load_mnist_data
 import matplotlib.pyplot as plt
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--epsilon', metavar='max-distance', default=0.3, type=float)
@@ -51,15 +52,23 @@ with tf.Session() as sess:
                      feed_dict={classifier.x_input: x_test_adv, classifier.y_input: y_test})
   print('robust classifier adv acc {}'.format(adv_acc))
 
+  plt.figure(figsize=(3.5*1.2,2*1.2))
+
   # Evaluate generative classifier
   attack = PGDAttackClassifier(classifier=bayes_classifier, loss_fn='cw', **attack_config)
   # x_test_adv_bayes = attack.batched_perturb(x_test, y_test, sess)
   x_test_adv_bayes = np.load(os.path.join(datadir, 'x_test_adv_detector.npy'))
   bayes_nat_accs = bayes_classifier.nat_accs(x_test, y_test, sess)
   bayes_adv_errors = bayes_classifier.adv_error(x_test_adv_bayes, y_test, sess)
-
-  plt.figure(figsize=(3.5*1.2,2*1.2))
   plt.plot(bayes_adv_errors, bayes_nat_accs, label='Generative classifier')
+
+  # Evaluate generative classifier - use cross-entropy loss
+  attack = PGDAttackClassifier(classifier=bayes_classifier, loss_fn='xent', **attack_config)
+  # x_test_adv_bayes = attack.batched_perturb(x_test, y_test, sess)
+  x_test_adv_bayes = np.load(os.path.join(datadir, 'x_test_adv_detector_xent.npy'))
+  bayes_nat_accs = bayes_classifier.nat_accs(x_test, y_test, sess)
+  bayes_adv_errors = bayes_classifier.adv_error(x_test_adv_bayes, y_test, sess)
+  plt.plot(bayes_adv_errors, bayes_nat_accs, label='Generative classifier (xent loss)')
 
   plt.ylim([0.9, 1.0])
   plt.xlabel('Error on perturbed MNIST test set')
